436743bf759b390173f750ac8ec1a5d474620c2e
Alexander Roshchin <alexander.roshchin85@gmail.com>
2020-09-13 11:28:26 +0300

    Add validation for user creation

--- server/routes/userRouter.js
+++ server/routes/userRouter.js
@@ -1,15 +1,13 @@
 const express = require('express')
 const {createUser, getUser, getAllUsers, updateUser, removeUser} = require('./../controllers/userController')
-const {validateUserForUpdate} = require('./../middleware/userValidation')
-
+const {validateUserOnCreate, validateUserForUpdate} = require('./../middleware/userValidation')
 
 const userRouter = express.Router()
 
-userRouter.post('/user', createUser)
+userRouter.post('/user', validateUserOnCreate, createUser)
 userRouter.get('/users/:userId', getUser)
 userRouter.get('/users', getAllUsers)
 userRouter.patch('/users/:userId', validateUserForUpdate, updateUser)
 userRouter.delete('/users/:userId', removeUser)
-
 
 module.exports = userRouter
